84ecf1d9a7983bd1460b86b280a9b350be18f73a
ZuuZaa <zumbaghirova@gmail.com>
2019-04-16 10:54:48 +0400

    form is ready

--- assets/gif.js
+++ assets/gif.js
@@ -1,30 +1,48 @@
-var animals = ["cat", "dog", "penquin", "rabbit","frog", "snake", "bird", "turtle", "horse", "pig", "monkey"]
+var emotions = ["happy", "sad", "impressed", "love", "upset", "suprised", "tired", "hate", "sunny", "friend", "true", "magic"]
 
-for (i in animals){
+
+for (i in emotions) {
     gifbtn = $("<button>").addClass("btn btn-info my-btn")
-    gifbtn.text(animals[i])
+    gifbtn.text(emotions[i])
     $("#btn-holder").append(gifbtn)
 }
-$(".my-btn").on("click", function() {
-    $.ajax( {
-        url: "https://api.giphy.com/v1/gifs/search" ,
+
+function clickButton() {
+    $.ajax({
+        url: "https://api.giphy.com/v1/gifs/search",
         data: {
             q: $(this).text(),
             api_key: "c4UCMdKemf33qzUJMnURnI50YjHBmCRT",
             limit: 9
         }
-        }).done(function (response){
-            $("#gif-holder").empty()
-              for (i=0; i<9; i++) {
-                  var gifimg = $("<img>")
-                  gifimg.addClass("mx-auto p-2")
-                  gifimg.attr("data-move", response.data[i].images.fixed_height.url)
-                  gifimg.attr("data-still", response.data[i].images.fixed_height_still.url)
-                  gifimg.attr("data-condition", "still")
-                  gifimg.attr("src", response.data[i].images.fixed_height_still.url)
-                  $("#gif-holder").append(gifimg)
-              }
+    }).done(function (response) {
+        $("#gif-holder").empty()
+        for (i = 0; i < 9; i++) {
+            var gifimg = $("<img>")
+            gifimg.addClass("mx-auto p-2")
+            gifimg.attr("data-move", response.data[i].images.fixed_height.url)
+            gifimg.attr("data-still", response.data[i].images.fixed_height_still.url)
+            gifimg.attr("data-condition", "still")
+            gifimg.attr("src", response.data[i].images.fixed_height_still.url)
+            $("#gif-holder").append(gifimg)
+        }
+        $("img").on("click", clickImage)
+    })
+}
 
-        })
+function clickImage() {
+    alert ("clicked")
+}
+
+$(".my-btn").on("click", clickButton)
+
+$("form").on("submit", function (e) {
+    e.preventDefault()
+    gifbtn = $("<button>").addClass("btn btn-info my-btn")
+    gifbtn.text($("#searchInput").val())
+    $("#btn-holder").append(gifbtn)
+    $(".my-btn").on("click", clickButton)
+    $("img").on("click", clickImage)
 })
+
 
